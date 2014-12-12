39a20027742d3706589c336e44a7f2c057c4dbe8
Binomial <jeremy_sc_hneiter@hotmail.fr>
2014-12-12 19:05:54 +0100

    Création des Console, GameBoard, Player

--- Main.cpp
+++ Main.cpp
@@ -6,13 +6,15 @@
  */
 
 #include <cstdlib>
-#include "header/anagramstring/Permut.h"
-#include "header/dawg/Trie.h"
-#include "header/anagramint/Dictionnary.h"
-#include "header/anagramint/FindWord.h"
+#include "header/controller/anagramstring/Permut.h"
+#include "header/controller/dawg/Trie.h"
+#include "header/controller/anagramint/Dictionnary.h"
+#include "header/controller/anagramint/FindWord.h"
+#include "header/model/Bundle.h"
+#include "header/model/GameBoard.h"
+#include "header/view/Console.h"
 
 using namespace std;
-
 
 void testAnagramString(string letters) {
     Permut p;
@@ -25,7 +27,7 @@ void testAnagramString(string letters) {
 void testAnagramInt(string letters) {
     Dictionnary dict;
     dict.importDictionnary();
-    
+
     FindWord fw(dict.getMap());
     fw.permut(letters);
     fw.displaySolution();
@@ -36,36 +38,58 @@ void testDawg(string letters) {
     dawg->loadDawg("./dict/dictFrDawg.dc");
     dawg->findWords(letters);
 }
+
+void testBundle() {
+    Bundle *b = new Bundle();
+
+    s_letter ll;
+
+    b->initBundle();
+
+    ll = b->takeLetter();
+    cout << ll.car << " pts : " << ll.point << endl;
+
+}
+
 /*
  * 
  */
 int main() {
     int choice;
     string letters;
-    do{
-        cout << endl << endl << "------ MENU ------" << endl<< endl;
-        cout << "0 - Exit" << endl  << "1 - Anagram Int" << endl << "2 - Anagram String" << endl << "3 - DAWG" << endl << endl;
+    do {
+        cout << endl << endl << "------ MENU ------" << endl << endl;
+        cout << "0 - Exit" << endl << "1 - Anagram Int" << endl << "2 - Anagram String" << endl << "3 - DAWG" << endl << endl;
         cin >> choice;
-    }while(choice < 0 || choice > 3);
-    
-    if(choice == 0) {
+    } while (choice < 0 || choice > 4);
+
+    if (choice == 0) {
         cout << "Goodbye marylou" << endl;
     } else {
-        
-        cout << "Vos lettres : ";
-        cin >> letters;
-        
-        switch(choice) {
+
+        switch (choice) {
             case 1:
+                cout << "Vos lettres : ";
+                cin >> letters;
                 testAnagramInt(letters);
                 break;
             case 2:
+                cout << "Vos lettres : ";
+                cin >> letters;
                 testAnagramString(letters);
                 break;
             case 3:
+                cout << "Vos lettres : ";
+                cin >> letters;
                 testDawg(letters);
                 break;
-                        
+            case 4:
+                Console *c = new Console();
+                GameBoard *gb = new GameBoard();
+                gb->initGameBoard();
+                c->display(*gb);
+                break;
+
         }
     }
     return 0;
